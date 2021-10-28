b007a29a0e1972ff3c6ec592d16b16da6bc838fa
JuYong <wndyd5577@naver.com>
2021-10-29 01:37:30 +0900

    12851

--- src/baekjoon/bfs/12851.py
+++ src/baekjoon/bfs/12851.py
@@ -1,8 +1,8 @@
 from collections import deque
 a, b = map(int, input().split())
 
-visit = [0] * 100001
-
+visit = [-1] * 100001
+visit[a] = 0
 queue = deque([a])
 weight = deque([0])
 cnt = 0
@@ -12,7 +12,7 @@ while queue:
         cnt += 1
 
     for x in [q+1, q-1, q*2]:
-        if 0 <= x < 100001 and (visit[x] == 0 or visit[q]+1 <= visit[x]):
+        if 0 <= x < 100001 and (visit[x] == -1 or visit[q]+1 <= visit[x]):
             queue.append(x)
             visit[x] = visit[q]+1
 print(visit[b])
